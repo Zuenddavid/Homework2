dabf6f68a3d4e52f8f18de8beef4f26eedcf004f
Zuenddavid <97689113+Zuenddavid@users.noreply.github.com>
2022-03-22 00:09:09 +0100

    Homework 2, original

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ url = "https://www.gutenberg.org/cache/epub/64317/pg64317.txt"
 local_name = "The_Great_Gatsby.txt"
 
 
-def save_locally():
+def save_locally(local_name,url):
     """
     Save the book locally, so we can use it faster and no need to load every time
     :return: None
@@ -15,14 +15,12 @@ def save_locally():
                 line = line.decode('utf-8-sig').replace("\n", "")
                 local_fp.write(line)
 
-save_locally()
-punctuation = ",;!.?-()"
+save_locally("Alice’s Adventures in Wonderland","https://www.gutenberg.org/files/11/11-0.txt")
+save_locally("A Tale of Two Cities","https://www.gutenberg.org/files/98/98-0.txt")
 
-def get_unique_words():
-    """
-    Get the dictionary of unique words and their frequency
-    :return: dict
-    """
+punctuation = ",;!.?-()"
+def get_unique_words(local_name):
+
     unique_words = {}
     word_count = 0
     with open(local_name) as fp:
@@ -35,11 +33,11 @@ def get_unique_words():
             for word in line.split():
                 unique_words[word] = unique_words.get(word, 0) + 1
                 word_count += 1
-    return unique_words, word_count
+    return unique_words,word_count
 
-unique_words, word_count = get_unique_words()
+unique_words, word_count = get_unique_words(local_name)
 
-def get_7_charachters():
+def get_7_charachters(local_nam):
 
     unique_words7 = []
     for word in unique_words.keys():
@@ -47,81 +45,33 @@ def get_7_charachters():
             unique_words7.append(word)
     return unique_words7
 
-unique_words7 = get_7_charachters()
+unique_words7 = get_7_charachters(local_name)
 
-unique_words7 = len(unique_words7)
-unique_word_1 = len(unique_words.keys())
-unique_ratio = unique_word_1/word_count
-
-from urllib.request import urlopen
-
-url2="https://www.gutenberg.org/files/98/98-0.txt"
-local_name2 = "A Tale of Two Cities"
-def save_locally2():
-    """
-    Save the book locally, so we can use it faster and no need to load every time
-    :return: None
-    """
-    with open(local_name2, "w") as local_fp2:
-        with urlopen(url2) as fp2:
-            for line2 in fp2:
-                line2 = line2.decode('utf-8-sig').replace("\n", "")
-                local_fp2.write(line2)
-
-save_locally2()
-
-punctuation = ",;!.?-()"
-def get_unique_words2():
-    """
-    Get the dictionary of unique words and their frequency
-    :return: dict
-    """
-    unique_words2 = {}
-    word_count2 = 0
-    with open(local_name2) as fp:
-        for line2 in fp:
-            # remove punctuation
-            for d in punctuation:
-                line2 = line2.replace(d, " ")
-            line2 = line2.lower()
-            # get the words:
-            for word2 in line2.split():
-                unique_words2[word2] = unique_words2.get(word2, 0) + 1
-                word_count2 += 1
-    return unique_words2, word_count2
-
-unique_words2, word_count2 = get_unique_words2()
-
-def get_7_charachters2():
-
-    unique_words7_2 = []
-    for word2 in unique_words2.keys():
-        if len(word2) >= 7:
-            unique_words7_2.append(word2)
-
-    return unique_words7_2
-
-unique_words7_2 = get_7_charachters2()
+def get_rank(B1,B2):
+    rank = []
+    if B1 > B2:
+        rank.append("more")
+    else:
+        rank.append("less")
+    return rank
 
 
-unique_words7_2 = len(unique_words7_2)
-unique_word_2 = len(unique_words2.keys())
-unique_ratio_2 = unique_word_2/word_count2
+UniquewordsB1, wordcountB1 = get_unique_words("The_Great_Gatsby.txt")
+More_7 = get_7_charachters("The_Great_Gatsby.txt")
+More_7B1 = len(More_7)
+lenuniquewordsB1 = len(UniquewordsB1.keys())
+unique_ratioB1 =lenuniquewordsB1/wordcountB1
+print(lenuniquewordsB1, wordcountB1, unique_ratioB1)
 
-rank = []
-if unique_word_1 > unique_word_2:
-    rank.append("more")
-else:
-    rank.append("less")
+UniquewordsB2, wordcountB2 = get_unique_words("A Tale of Two Cities")
+More_72 = get_7_charachters("A Tale of Two Cities")
+More_7B2 = len(More_72)
+lenunqiuewordsB2 = len(UniquewordsB2.keys())
+unique_ratioB2 =  lenunqiuewordsB2/ wordcountB2
+print(lenunqiuewordsB2, wordcountB2, unique_ratioB2)
 
-rank2 = []
-if unique_words7 > unique_words7_2:
-    rank2.append('more')
-else:
-    rank2.append('less')
-
-print(f" Author of Book 1 has a vocabulary of {unique_word_1} words, whereas author of book 2 has a vocabulary of {unique_word_2} words\n"
-      f" Therefore, the Author of Book 1 has {rank} vocabulary than author of book 2\n"
-      f" Book 1 has {unique_words7} unique words with more than 7 characters whereas book 2 has {unique_words7_2}\n"
-      f" Therefore, Book 1 has {rank2} unique words than book 2\n"
-      f" The ratio between distinct words and total number of words in book 1 is {round(unique_ratio,4)} whereas {round(unique_ratio_2,4)} in book 2.")
+print(f" Author of Book 1 has a vocabulary of {lenuniquewordsB1} words, whereas author of book 2 has a vocabulary of {lenunqiuewordsB2} words\n"
+      f" Therefore, the Author of Book 1 has {get_rank(lenuniquewordsB1,lenunqiuewordsB2)} vocabulary than author of book 2\n"
+      f" Book 1 has {More_7B1} unique words with more than 7 characters whereas book 2 has {More_7B2}\n"
+      f" Therefore, Book 1 has {get_rank(More_7B1,More_7B2)} unique words than book 2\n"
+      f" The ratio between distinct words and total number of words in book 1 is {round(unique_ratioB1,4)} whereas {round(unique_ratioB2,4)} in book 2.")
